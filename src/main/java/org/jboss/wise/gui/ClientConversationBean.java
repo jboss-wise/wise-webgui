71081dbd53c8a4e99a7c76d0285543d34443a354
alessio.soldano@jboss.com <alessio.soldano@jboss.com@a17ab928-1b57-0410-8599-cedcd8a065c6>
2013-02-19 15:10:22 +0000

    Minor refactoring

--- src/main/java/org/jboss/wise/gui/ClientConversationBean.java
+++ src/main/java/org/jboss/wise/gui/ClientConversationBean.java
@@ -19,39 +19,25 @@ package org.jboss.wise.gui;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.Serializable;
-import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.StringTokenizer;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.jws.soap.SOAPBinding;
 
 import org.jboss.logging.Logger;
 import org.jboss.wise.core.client.InvocationResult;
 import org.jboss.wise.core.client.WSDynamicClient;
-import org.jboss.wise.core.client.WSEndpoint;
 import org.jboss.wise.core.client.WSMethod;
-import org.jboss.wise.core.client.WSService;
-import org.jboss.wise.core.client.WebParameter;
 import org.jboss.wise.core.client.builder.WSDynamicClientBuilder;
 import org.jboss.wise.core.client.impl.reflection.builder.ReflectionBasedWSDynamicClientBuilder;
 import org.jboss.wise.core.exception.InvocationException;
-import org.jboss.wise.core.exception.WiseRuntimeException;
 import org.jboss.wise.core.utils.JBossLoggingOutputStream;
 import org.jboss.wise.gui.treeElement.GroupWiseTreeElement;
 import org.jboss.wise.gui.treeElement.LazyLoadWiseTreeElement;
 import org.jboss.wise.gui.treeElement.WiseTreeElement;
-import org.jboss.wise.gui.treeElement.WiseTreeElementBuilder;
 import org.richfaces.component.UITree;
 import org.richfaces.event.ItemChangeEvent;
 import org.richfaces.model.TreeNodeImpl;
@@ -60,7 +46,7 @@ import org.richfaces.model.TreeNodeImpl;
 @ConversationScoped
 public class ClientConversationBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -3778997821476776895L;
     
     private static final int CONVERSATION_TIMEOUT = 15 * 60 * 1000; //15 mins instead of default 30 mins
     private static CleanupTask<WSDynamicClient> cleanupTask = new CleanupTask<WSDynamicClient>(true);
@@ -113,7 +99,7 @@ public class ClientConversationBean implements Serializable {
 	}
 	if (client != null) {
 	    try {
-		services = convertServicesToGui(client.processServices());
+		services = ClientHelper.convertServicesToGui(client.processServices());
 	    } catch (Exception e) {
 		error = "Could not parse WSDL from specified URL. Please check logs for further information.";
 		logException(e);
@@ -125,14 +111,10 @@ public class ClientConversationBean implements Serializable {
 	if (currentOperation == null) return;
 	outputTree = null;
 	error = null;
-	StringTokenizer st = new StringTokenizer(currentOperation, ";");
-	String serviceName = st.nextToken();
-	String portName = st.nextToken();
-	String operationName = st.nextToken();
 	try {
-	    inputTree = convertOperationParametersToGui(client.getWSMethod(serviceName, portName, operationName), client);
+	    inputTree = ClientHelper.convertOperationParametersToGui(ClientHelper.getWSMethod(currentOperation, client), client);
 	} catch (Exception e) {
-	    error = toErrorMessage(e);
+	    error = ClientHelper.toErrorMessage(e);
 	    logException(e);
 	}
     }
@@ -140,52 +122,34 @@ public class ClientConversationBean implements Serializable {
     public void performInvocation() {
 	outputTree = null;
 	error = null;
-	StringTokenizer st = new StringTokenizer(currentOperation, ";");
-	String serviceName = st.nextToken();
-	String portName = st.nextToken();
-	String operationName = st.nextToken();
 	try {
-	    WSMethod wsMethod = client.getWSMethod(serviceName, portName, operationName);
-	    Map<String, Object> params = new HashMap<String, Object>();
-	    for (Iterator<Object> it = inputTree.getChildrenKeysIterator(); it.hasNext(); ) {
-		WiseTreeElement wte = (WiseTreeElement)inputTree.getChild(it.next());
-		params.put(wte.getName(), wte.isNil() ? null : wte.toObject());
-	    }
+	    WSMethod wsMethod = ClientHelper.getWSMethod(currentOperation, client);
 	    InvocationResult result = null;
 	    try {
-		result = wsMethod.invoke(params);
+		result = wsMethod.invoke(ClientHelper.processGUIParameters(inputTree));
 	    } catch (InvocationException e) {
 		logException(e);
 		error = "Unexpected fault / error received from target endpoint";
 	    }
 	    if (result != null) {
-		outputTree = convertOperationResultToGui(result, client);
+		outputTree = ClientHelper.convertOperationResultToGui(result, client);
 		error = null;
 	    }
 	} catch (Exception e) {
-	    error = toErrorMessage(e);
+	    error = ClientHelper.toErrorMessage(e);
 	    logException(e);
 	}
     }
     
     public void generateRequestPreview() {
 	requestPreview = null;
-	StringTokenizer st = new StringTokenizer(currentOperation, ";");
-	String serviceName = st.nextToken();
-	String portName = st.nextToken();
-	String operationName = st.nextToken();
 	try {
-	    WSMethod wsMethod = client.getWSMethod(serviceName, portName, operationName);
-	    Map<String, Object> params = new HashMap<String, Object>();
-	    for (Iterator<Object> it = inputTree.getChildrenKeysIterator(); it.hasNext(); ) {
-		WiseTreeElement wte = (WiseTreeElement)inputTree.getChild(it.next());
-		params.put(wte.getName(), wte.isNil() ? null : wte.toObject());
-	    }
+	    WSMethod wsMethod = ClientHelper.getWSMethod(currentOperation, client);
 	    ByteArrayOutputStream os = new ByteArrayOutputStream();
-	    wsMethod.writeRequestPreview(params, os);
+	    wsMethod.writeRequestPreview(ClientHelper.processGUIParameters(inputTree), os);
 	    requestPreview = os.toString("UTF-8");
 	} catch (Exception e) {
-	    requestPreview = toErrorMessage(e);
+	    requestPreview = ClientHelper.toErrorMessage(e);
 	    logException(e);
 	}
     }
@@ -202,93 +166,13 @@ public class ClientConversationBean implements Serializable {
 	try {
 	    el.resolveReference();
 	} catch (Exception e) {
-	    error = toErrorMessage(e);
+	    error = ClientHelper.toErrorMessage(e);
 	    logException(e);
 	}
     }
     
     public void onInputFocus(WiseTreeElement el) {
 	el.setNotNil(true);
-    }
-    
-    private static TreeNodeImpl convertOperationParametersToGui(WSMethod wsMethod, WSDynamicClient client) {
-	WiseTreeElementBuilder builder = new WiseTreeElementBuilder(client, true);
-	TreeNodeImpl rootElement = new TreeNodeImpl();
-	Collection<? extends WebParameter> parameters = wsMethod.getWebParams().values();
-	SOAPBinding soapBindingAnn = wsMethod.getEndpoint().getUnderlyingObjectClass().getAnnotation(SOAPBinding.class);
-	boolean rpcLit = false;
-	if (soapBindingAnn != null) {
-	    SOAPBinding.Style style = soapBindingAnn.style();
-	    rpcLit = style != null && SOAPBinding.Style.RPC.equals(style);
-	}
-	for (WebParameter parameter : parameters) {
-	    WiseTreeElement wte = builder.buildTreeFromType(parameter.getType(), parameter.getName(), null, !rpcLit);
-	    rootElement.addChild(wte.getId(), wte);
-	}
-	return rootElement;
-    }
-    
-    private static TreeNodeImpl convertOperationResultToGui(InvocationResult result,  WSDynamicClient client) {
-	WiseTreeElementBuilder builder = new WiseTreeElementBuilder(client, false);
-	TreeNodeImpl rootElement = new TreeNodeImpl();
-	Map<String, Type> resTypes = new HashMap<String, Type>();
-	for (Entry<String, Object> res : result.getResult().entrySet()) {
-	    String key = res.getKey();
-	    if (key.startsWith(WSMethod.TYPE_PREFIX)) {
-		resTypes.put(key, (Type)res.getValue());
-	    }
-	}
-	for (Entry<String, Object> res : result.getResult().entrySet()) {
-	    final String key = res.getKey();
-	    if (!key.startsWith(WSMethod.TYPE_PREFIX)) {
-		Type type = resTypes.get(WSMethod.TYPE_PREFIX + key);
-		if (type != void.class && type != Void.class) {
-		    WiseTreeElement wte = builder.buildTreeFromType(type, key, res.getValue(), true);
-		    rootElement.addChild(wte.getId(), wte);
-		}
-	    }
-	}
-	return rootElement;
-    }
-    
-    private static List<Service> convertServicesToGui(Map<String, WSService> servicesMap) {
-	List<Service> services = new LinkedList<Service>();
-	for (Entry<String, WSService> serviceEntry : servicesMap.entrySet()) {
-	    Service service = new Service();
-	    services.add(service);
-	    service.setName(serviceEntry.getKey());
-	    List<Port> ports = new LinkedList<Port>();
-	    service.setPorts(ports);
-	    for (Entry<String, WSEndpoint> endpointEntry : serviceEntry.getValue().processEndpoints().entrySet()) {
-		Port port = new Port();
-		port.setName(endpointEntry.getKey());
-		ports.add(port);
-		List<Operation> operations = new LinkedList<Operation>();
-		port.setOperations(operations);
-		for (Entry<String, WSMethod> methodEntry : endpointEntry.getValue().getWSMethods().entrySet()) {
-		    Operation operation = new Operation();
-		    operation.setName(methodEntry.getKey());
-		    StringBuilder sb = new StringBuilder();
-		    sb.append(methodEntry.getKey());
-		    sb.append("(");
-		    Iterator<? extends WebParameter> paramIt = methodEntry.getValue().getWebParams().values().iterator();
-		    while (paramIt.hasNext()) {
-			WebParameter param = paramIt.next();
-			Type type = param.getType();
-			sb.append(type instanceof Class<?> ? ((Class<?>)type).getSimpleName() : type.toString());
-			sb.append(" ");
-			sb.append(param.getName());
-			if (paramIt.hasNext()) {
-			    sb.append(", ");
-			}
-		    }
-		    sb.append(")");
-		    operation.setFullName(sb.toString());
-		    operations.add(operation);
-		}
-	    }
-	}
-	return services;
     }
     
     public void updateCurrentOperation(ItemChangeEvent event){
@@ -392,21 +276,6 @@ public class ClientConversationBean implements Serializable {
         this.requestPreview = requestPreview;
     }
 
-    private static String toErrorMessage(Exception e) {
-	StringBuilder sb = new StringBuilder();
-	if (e instanceof WiseRuntimeException) {
-	    sb.append(e.getMessage());
-	} else {
-	    sb.append(e.toString());
-	}
-	if (e.getCause() != null) {
-	    sb.append(", caused by ");
-	    sb.append(e.getCause());
-	}
-	sb.append(". Please check logs for further information.");
-	return sb.toString();
-    }
-    
     private static void logException(Exception e) {
 	logger.error("", e);
     }
